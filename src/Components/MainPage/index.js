0f9cc5d0b221ed7a6df3f5ca10045e21fee51ce1
AvanthikaMeenakshi <avanthika157@gmail.com>
2018-12-14 21:42:21 +0530

    Drag and drop for top and bottom text

--- src/Components/MainPage/index.js
+++ src/Components/MainPage/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Gallery from 'react-photo-gallery';
-import { Modal, ModalHeader, ModalBody, FormGroup, Label, Input } from 'reactstrap';
-
+import { Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
 
 const photos = [
   { src: '/images/vict-baby.png', width: 4, height: 3 },
@@ -10,6 +9,16 @@ const photos = [
   { src: '/images/trump.jpg', width: 4, height: 3 }
 ];
 
+const initialState = {
+  toptext: "",
+  bottomtext: "",
+  // isTopDragging: false,
+  // isBottomDragging: false,
+  topY: "10%",
+  topX: "50%",
+  bottomX: "50%",
+  bottomY: "90%"
+}
 
 class MainPage extends React.Component {
   constructor() {
@@ -17,26 +26,15 @@ class MainPage extends React.Component {
     this.state = {
       currentImage: 0,
       modalIsOpen: false,
-      toptext: "",
-      bottomtext: ""
+      ...initialState
     };
-  }
-
-  changeText = (event) => {
-    console.log(event.currentTarget.name, event.currentTarget.value)
-    this.setState({
-      [event.currentTarget.name]: event.currentTarget.value
-    }, () => {
-      this.drawImageInCanvas();
-    });
   }
 
   openImage = (event, obj) => {
     this.setState(prevState => ({
       currentImage: obj.index,
-      toptext: "",
-      bottomtext: "",
-      modalIsOpen: !prevState.modalIsOpen
+      modalIsOpen: !prevState.modalIsOpen,
+      ...initialState
     }));
   }
 
@@ -46,35 +44,117 @@ class MainPage extends React.Component {
     }));
   }
 
-  drawImageInCanvas = () => {
-    const canvas = document.getElementById('image');
-    if (canvas) {
-      const context = canvas.getContext('2d');
-      const image = photos[this.state.currentImage];
-      const base_image = new Image();
-      base_image.src = image.src;
-      var wrh = base_image.width / base_image.height;
-      var newWidth = canvas.width;
-      var newHeight = newWidth / wrh;
-      const { toptext, bottomtext } = this.state;
-      console.log(toptext)
-      base_image.onload = function(){
-        context.canvas.height = newHeight;
-        context.drawImage(base_image, 0, 0, newWidth, newHeight);
-        context.font = "55px Impact";
-        context.fillText(toptext, 0, 50);
+  changeText = (event) => {
+    this.setState({
+      [event.currentTarget.name]: event.currentTarget.value
+    });
+  }
+
+  getStateObj = (e, type) => {
+    let rect = this.imageRef.getBoundingClientRect();
+    const xOffset = e.clientX - rect.left;
+    const yOffset = e.clientY - rect.top;
+    let stateObj = {};
+    if (type === "bottom") {
+      stateObj = {
+        isBottomDragging: true,
+        isTopDragging: false,
+        bottomX: `${xOffset}px`,
+        bottomY: `${yOffset}px`
+      }
+    } else if (type === "top") {
+      stateObj = {
+        isTopDragging: true,
+        isBottomDragging: false,
+        topX: `${xOffset}px`,
+        topY: `${yOffset}px`
       }
     }
+    return stateObj;
+  }
+
+  handleMouseDown = (e, type) => {
+    const stateObj = this.getStateObj(e, type);
+    document.addEventListener('mousemove', (event) => this.handleMouseMove(event, type));
+    this.setState({
+      ...stateObj
+    })
+  }
+
+  handleMouseMove = (e, type) => {
+    if (this.state.isTopDragging || this.state.isBottomDragging) {
+      let stateObj = {};
+      if (type === "bottom" && this.state.isBottomDragging) {
+        stateObj = this.getStateObj(e, type);
+      } else if (type === "top" && this.state.isTopDragging){
+        stateObj = this.getStateObj(e, type);
+      }
+      this.setState({
+        ...stateObj
+      });
+    }
+  };
+
+  handleMouseUp = (event) => {
+    document.removeEventListener('mousemove', this.handleMouseMove);
+    this.setState({
+      isTopDragging: false,
+      isBottomDragging: false
+    });
   }
 
   render() {
+    const image = photos[this.state.currentImage];
+    const base_image = new Image();
+    base_image.src = image.src;
+    var wrh = base_image.width / base_image.height;
+    var newWidth = 600;
+    var newHeight = newWidth / wrh;
+    const textStyle = {
+      fontFamily: "Impact",
+      fontSize: "50px",
+      userSelect: "none"
+    }
     return (
       <div>
         <Gallery photos={photos} onClick={this.openImage} />
-        <Modal className="meme-gen-modal" onOpened={this.drawImageInCanvas} isOpen={this.state.modalIsOpen}>
-          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+        <Modal className="meme-gen-modal" isOpen={this.state.modalIsOpen}>
+          <ModalHeader toggle={this.toggle}>Make-a-Meme</ModalHeader>
           <ModalBody>
-            <canvas width="600" id="image"></canvas>
+            <svg
+              width={newWidth}
+              height={newHeight}
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnshlink="http://www.w3.org/1999/xlink">
+              <image
+                ref={el => { this.imageRef = el }}
+                xlinkHref={photos[this.state.currentImage].src}
+                height={newHeight}
+                width={newWidth}
+              />
+              <text
+                style={{ ...textStyle, zIndex: this.state.isTopDragging ? 4 : 1 }}
+                x={this.state.topX}
+                y={this.state.topY}
+                dominantBaseline="middle"
+                textAnchor="middle"
+                onMouseDown={event => this.handleMouseDown(event, 'top')}
+                onMouseUp={event => this.handleMouseUp(event, 'top')}
+              >
+                  {this.state.toptext}
+              </text>
+              <text
+                style={textStyle}
+                dominantBaseline="middle"
+                textAnchor="middle"
+                x={this.state.bottomX}
+                y={this.state.bottomY}
+                onMouseDown={event => this.handleMouseDown(event, 'bottom')}
+                onMouseUp={event => this.handleMouseUp(event, 'bottom')}
+              >
+                  {this.state.bottomtext}
+              </text>
+            </svg>
           </ModalBody>
           <FormGroup>
             <Label for="toptext">Top Text</Label>
